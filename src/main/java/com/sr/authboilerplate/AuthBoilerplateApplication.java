4e06c9736e0a569e11bdb975749908c9a8e47b23
seungriyou <seungri0826@gmail.com>
2024-02-24 16:33:22 +0900

    chore: add timezone and locale setting

--- src/main/java/com/sr/authboilerplate/AuthBoilerplateApplication.java
+++ src/main/java/com/sr/authboilerplate/AuthBoilerplateApplication.java
@@ -1,13 +1,22 @@
 package com.sr.authboilerplate;
 
+import jakarta.annotation.PostConstruct;
+import java.util.Locale;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthBoilerplateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthBoilerplateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthBoilerplateApplication.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        Locale.setDefault(Locale.KOREA);
+    }
 
 }
