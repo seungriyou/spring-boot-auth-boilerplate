8728259ce2d5317f764617adb92f569602e44333
seungriyou <seungri0826@gmail.com>
2024-02-24 16:11:49 +0900

    feat: add auditing

---
+++ src/main/java/com/sr/authboilerplate/global/config/db/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.sr.authboilerplate.global.config.db;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
